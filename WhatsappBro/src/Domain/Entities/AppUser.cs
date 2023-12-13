408f1914d039c3541ddbad426bc3fab798093d8c
serkana607@gmail.com <serkana607@gmail.com>
2023-12-13 18:07:54 +0300

    Wrong DB Design

---
+++ WhatsappBro/src/Domain/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class AppUser : IdentityUser<Guid>, IEntity
+    {
+        public DateTime CreatedDate { get; set; }
+    }
+}
